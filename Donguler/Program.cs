201604bf51425026d5f66271b4c742895c25134a
utkue <utkue@DESKTOP-SGKUSO5>
2022-09-12 00:05:51 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "yazılım geliştirici yetiştirme kampı";
+            string kurs2 = "programlamaya başlangıç için temel kurs";
+            string kurs3 = "java";
+            string kurs4 = "python";
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+            Console.WriteLine();
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "yazılım geliştirici yetiştirme kampı" ,
+                "programlamaya başlangıç için temel kurs","java","python" };
+
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+            Console.WriteLine();
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KampIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string isim = "utku";
+            int sayi = 7;
+
+            Console.WriteLine(isim);
+            Console.WriteLine(sayi);
+
+            double dolardun = 17.55;
+            double dolarbugun = 18.25;
+
+            if (dolarbugun<dolardun)
+            {
+                Console.WriteLine("azalış butonu");
+            }
+
+            else if (dolarbugun>dolardun)
+            {
+                Console.WriteLine("artış butonu");
+            }
+
+            else
+            {
+                Console.WriteLine("eşit butonu");
+            }
+
+            bool sistemgiriskontrol = true;
+
+            if (sistemgiriskontrol == true)
+            {
+                Console.WriteLine("ayarlar arayüz butonu");
+            }
+            else
+            {
+                Console.WriteLine("çıkış yap butonu");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
